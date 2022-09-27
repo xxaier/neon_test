aa3c3ab18a2c67614f0b5071ace56a13e5c68589
any <any@user.tax>
2022-09-27 16:22:25 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@ use napi_derive::napi;
 
 #[napi]
 pub fn bin_u64(bin: Buffer) -> i64 {
-  u64::from_le_bytes(&bin[..]).unwrap() as _
+  u64::from_le_bytes(bin[..].try_into().unwrap()) as _
 }
 
 #[napi]
 pub fn u64_bin(i: i64) -> Buffer {
-  (i as u64).to_le_bytes().into()
+  (i as u64).to_le_bytes()[..].into()
 }
