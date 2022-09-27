04865fe5a097bee477dda7fe14f9239edc3f11f9
any <any@user.tax>
2022-09-27 16:39:03 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ use napi::bindgen_prelude::Buffer;
 use napi_derive::napi;
 
 #[napi]
-pub fn bin_u64(bin: Buffer) -> i64 {
+pub fn bin_i64(bin: Buffer) -> i64 {
   i64::from_le_bytes(bin[..].try_into().unwrap())
 }
 
 #[napi]
-pub fn u64_bin(i: i64) -> Buffer {
+pub fn i64_bin(i: i64) -> Buffer {
   (i as i64).to_le_bytes()[..].into()
 }
