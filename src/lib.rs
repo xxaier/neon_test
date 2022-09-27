177c57642eb1cd37c5f72b590d5c02538a54568b
any <any@user.tax>
2022-09-27 16:21:28 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 use napi::bindgen_prelude::Buffer;
 use napi_derive::napi;
-use ordered_varint::Variable;
 
 #[napi]
 pub fn bin_u64(bin: Buffer) -> i64 {
-  u64::decode_variable(&bin[..]).unwrap() as _
+  u64::from_le_bytes(&bin[..]).unwrap() as _
 }
 
 #[napi]
 pub fn u64_bin(i: i64) -> Buffer {
-  Buffer::from((i as u64).to_variable_vec().unwrap())
+  (i as u64).to_le_bytes().into()
 }
